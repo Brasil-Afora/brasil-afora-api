30039eb03c9e5d7483d4a6a0f96ef84313e725dd
Heitor Rodrigues Carvalho <heitorrodrigues2909@gmail.com>
2026-04-08 09:15:08 -0300

    remove auth middleware from get routes

--- src/http/routes/get-national-opportunities.ts
+++ src/http/routes/get-national-opportunities.ts
@@ -1,10 +1,8 @@
 import { getNationalOpportunities as getNationalOpportunitiesFunction } from "@/functions/get-national-opportunities"
-import { authMiddleware } from "@/http/middleware/auth"
 import { Hono } from "hono"
 
 export const getNationalOpportunities = new Hono().get(
   "/national-opportunities",
-  authMiddleware,
   async (c) => {
     const { nationalOpportunities, success } =
       await getNationalOpportunitiesFunction()
--- src/http/routes/get-opportunities.ts
+++ src/http/routes/get-opportunities.ts
@@ -1,17 +1,12 @@
 import { getOpportunities as getOpportunitiesFunction } from "@/functions/get-opportunities"
-import { authMiddleware } from "@/http/middleware/auth"
 import { Hono } from "hono"
 
-export const getOpportunities = new Hono().get(
-  "/opportunities",
-  authMiddleware,
-  async (c) => {
-    const { opportunities, success } = await getOpportunitiesFunction()
+export const getOpportunities = new Hono().get("/opportunities", async (c) => {
+  const { opportunities, success } = await getOpportunitiesFunction()
 
-    if (!success) {
-      return c.json({ message: "Failed to fetch opportunities." }, 500)
-    }
-
-    return c.json({ opportunities }, 200)
+  if (!success) {
+    return c.json({ message: "Failed to fetch opportunities." }, 500)
   }
-)
+
+  return c.json({ opportunities }, 200)
+})
